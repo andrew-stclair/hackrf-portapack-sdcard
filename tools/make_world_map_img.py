562cd667f43a1303716d4869d4c94d81fdcba88f
Andrew St Clair <andrew_stclair95@hotmail.com>
2023-07-05 05:11:26 +0000

    Swap pyvips for pillow

--- tools/make_world_map_img.py
+++ tools/make_world_map_img.py
@@ -1,7 +1,7 @@
 """Make the map image"""
 import os
 from time import sleep
-import pyvips
+from PIL import Image
 import requests
 
 ZOOM = 7
@@ -27,8 +27,36 @@ X_PIXELS = X_TILES*256
 Y_PIXELS = Y_TILES*256
 print("Pixels:", X_PIXELS, Y_PIXELS)
 
+def join_tiled_images(image_paths, x_tiles, y_tiles, output_path):
+    """Function to join map tiles together into one image"""
+    # Open the first tile to get the size
+    first_tile = Image.open(image_paths[0])
+    tile_width, tile_height = first_tile.size
+
+    # Calculate the total size of the joined image
+    joined_width = tile_width * x_tiles
+    joined_height = tile_height * y_tiles
+
+    # Create a new blank image for the joined image
+    joined_image = Image.new('RGB', (joined_width, joined_height))
+
+    # Iterate over the tiles and paste them into the joined image
+    x_count = 0
+    y_count = 0
+    for tile_path in image_paths:
+        tile = Image.open(tile_path)
+        joined_image.paste(tile, (y_count * tile_height, x_count * tile_width))
+        x_count += 1
+        if x_count >= x_tiles:
+            x_count = 0
+            y_count += 1
+
+    # Save the joined image
+    joined_image.save(output_path)
+
 # Download Tiles
 print("Downloading Tiles")
+image_array = []
 for x in range(X_TILES):
     for y in range(Y_TILES):
         print("Downloading:", x, y)
@@ -38,13 +66,9 @@ for x in range(X_TILES):
             os.makedirs(f"/tmp/map-tiles/{x}")
         with open(f"/tmp/map-tiles/{x}/{y}.png", "wb") as image:
             image.write(r_image.content)
-        sleep(0.25)
+            image_array.append(f"/tmp/map-tiles/{x}/{y}.png")
+        sleep(0.01)
 
 # Make Map
 print("Making image array")
-tiles = [pyvips.Image.new_from_file(f"/tmp/map-tiles/{x}/{y}.png", access="sequential")
-         for y in range(Y_TILES) for x in range(X_TILES)]
-print("Joining array into single image")
-image = pyvips.Image.arrayjoin(tiles, across=X_TILES)
-print("Saving image")
-image.write_to_file("sdcard/ADSB/world_map.png")
+join_tiled_images(image_array, X_TILES, Y_TILES, "sdcard/ADSB/world_map.png")
